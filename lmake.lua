1cf9342993504d3f66ba3adfd91f3151a13b5dc1
UnaiFernandez <fernandezunai027@gmail.com>
2020-10-28 18:15:45 +0100

    lmakefile done (grande galardo)

---
+++ lmake.lua
@@ -0,0 +1,22 @@
+lmake_compatibility_version(1)
+function build()
+    lmake_set_compiler("/bin/gcc")
+    lmake_set_compiler_flags("-pthread -Wall -Iinclude")
+    lmake_set_compiler_out("binaries/%.o")
+    lmake_compile("main.c sched_disp.c timer.c clock.c p_generator.c")
+    lmake_set_linker("/bin/gcc")
+    lmake_set_linker_flags("-pthread")
+    lmake_set_linker_out("binaries/seso")
+    lmake_link("binaries/main.c.o binaries/sched_disp.c.o binaries/timer.c.o binaries/clock.c.o binaries/p_generator.c.o")
+end
+function exec()
+    build()
+    lmake_exec("binaries/seso -p 10 -m 5000000 -t 40")
+end
+function clean()
+    lmake_exec("rm binaries/clock.c.o") 
+    lmake_exec("rm binaries/main.c.o") 
+    lmake_exec("rm binaries/p_generator.c.o") 
+    lmake_exec("rm binaries/sched_disp.c.o") 
+    lmake_exec("rm binaries/timer.c.o")
+end    
